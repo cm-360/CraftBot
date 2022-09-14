c450f48ad5a686928ea300c603db71621b0d22c1
cm360 <cm360channel@gmail.com>
2022-09-14 16:40:32 -0400

    Python-ify strings

--- cogs/control.py
+++ cogs/control.py
@@ -11,25 +11,25 @@ import os
 
 
 class ControlCog(commands.Cog):
-    env_guild_ids = [int(os.environ["CRAFTBOT_GUILD_ID"])]
+    env_guild_ids = [int(os.environ['CRAFTBOT_GUILD_ID'])]
 
     def __init__(self, bot: craftbot.CraftBot):
         self.bot = bot
 
-    @slash_command(description="Reload this bot's cogs.", guild_ids=env_guild_ids)
+    @slash_command(description='Reload this bot\'s cogs.', guild_ids=env_guild_ids)
     @commands.check(craftbot.CraftBot.is_admin)
     async def reload_cogs(self, ctx: discord.ApplicationContext):
         if self.bot.reload_cogs():
-            await ctx.interaction.response.send_message(content="Reloaded all cogs!")
+            await ctx.interaction.response.send_message(content='Reloaded all cogs!')
         else:
-            await ctx.interaction.response.send_message(content="An error occured while reloading cogs, check the log for details.")
+            await ctx.interaction.response.send_message(content='An error occured while reloading cogs, check the log for details.')
 
     @reload_cogs.error
     async def reload_cogs_error(self, ctx: discord.ApplicationContext, error):
         if isinstance(error, commands.CheckFailure):
-            await ctx.interaction.response.send_message(content="You do not have permission to run this command!")
+            await ctx.interaction.response.send_message(content='You do not have permission to run this command!')
         else:
-            await ctx.interaction.response.send_message(content="An unspecified error has occured. Please check the log for details.")
+            await ctx.interaction.response.send_message(content='An unspecified error has occured. Please check the log for details.')
 
 
 def setup(bot):
--- cogs/thread.py
+++ cogs/thread.py
@@ -11,30 +11,30 @@ import os
 
 
 class ThreadCog(commands.Cog):
-    env_guild_ids = [int(os.environ["CRAFTBOT_GUILD_ID"])]
+    env_guild_ids = [int(os.environ['CRAFTBOT_GUILD_ID'])]
 
     def __init__(self, bot: craftbot.CraftBot):
         self.bot = bot
     
-    group_thread = discord.SlashCommandGroup(name="thread", description="Commands for managing threads.", guild_ids=env_guild_ids)
+    group_thread = discord.SlashCommandGroup(name='thread', description='Commands for managing threads.', guild_ids=env_guild_ids)
 
-    @group_thread.command(description="Rename the current thread.", guild_ids=env_guild_ids)
+    @group_thread.command(description='Rename the current thread.', guild_ids=env_guild_ids)
     @commands.check(craftbot.CraftBot.is_admin)
-    async def rename(self, ctx: discord.ApplicationContext, name: Option(str, "The new name for this thread.", required=True)):
+    async def rename(self, ctx: discord.ApplicationContext, name: Option(str, 'The new name for this thread.', required=True)):
         if type(ctx.channel) is discord.Thread:
             await ctx.interaction.channel.edit(name=name)
-            await ctx.interaction.response.send_message(content="This thread has been renamed.")
+            await ctx.interaction.response.send_message(content='This thread has been renamed.')
         else:
-            await ctx.interaction.response.send_message(content="Sorry, this command can only be used inside of a thread!")
+            await ctx.interaction.response.send_message(content='Sorry, this command can only be used inside of a thread!')
 
-    @group_thread.command(description="Archive the current thread.", guild_ids=env_guild_ids)
+    @group_thread.command(description='Archive the current thread.', guild_ids=env_guild_ids)
     @commands.check(craftbot.CraftBot.is_admin)
     async def archive(self, ctx: discord.ApplicationContext):
         if type(ctx.channel) is discord.Thread:
-            await ctx.interaction.response.send_message(content="This thread will be archived.")
+            await ctx.interaction.response.send_message(content='This thread will be archived.')
             await ctx.interaction.channel.archive()
         else:
-            await ctx.interaction.response.send_message(content="Sorry, this command can only be used inside of a thread!")
+            await ctx.interaction.response.send_message(content='Sorry, this command can only be used inside of a thread!')
 
 
 def setup(bot):
